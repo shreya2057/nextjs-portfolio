2528f5ccdf1097c5a263f8f016f9281cec5f10f4
shreya <shreyashrestha2k57@gmail.com>
2024-07-15 11:51:57 +0545

    fix: home page image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { AvatarImage } from "../assets/images";
 import { PrimaryButton } from "../components/Buttons/PrimaryButton";
+import { CodingBrowser, CodingIcon } from "../assets/images";
 
 export default function Home() {
   const router = useRouter();
   return (
     <div className="flex max-h-min w-full bg-black px-16 items-center py-10 relative overflow-hidden">
-      <div className="flex justify-between w-full ">
+      <div className="flex justify-between w-full gap-20">
         <div className="flex flex-1 gap-5 flex-col justify-center">
           <div className="flex gap-4">
             <div className=" text-amber-600 font-bold text-5xl">Hello!!!</div>
           </div>
-          <div className="text-green-400">
+          <div className="text-green-400 text-justify">
             I am a front end web developer who is passionate learner with
             expertise in HTML, CSS, JavaScript, React and CSS frameworks. I
             excel at designing responsive and dyanmic website using various
@@ -29,13 +29,22 @@ export default function Home() {
             }}
           />
         </div>
-        <div className="flex-1 flex justify-center w-full">
-          <Image
-            src={AvatarImage}
-            className="z-10"
-            alt="Avatar image"
-            height={300}
-          />
+        <div className="flex-1 flex justify-center items-center">
+          <div className=" w-48 h-48 relative">
+            <Image
+              src={CodingBrowser}
+              className="absolute"
+              alt="Avatar image"
+              width={200}
+              height={150}
+            />
+            <Image
+              className="absolute -bottom-10 -left-10"
+              src={CodingIcon}
+              alt="Avatar image"
+              height={150}
+            />
+          </div>
         </div>
       </div>
     </div>
