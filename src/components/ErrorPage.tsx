d9afadde24d54d9a002c4efdc2045e24f0f81e10
shreya <shreyashrestha2k57@gmail.com>
2024-08-13 15:26:59 +0545

    feat: not found page ui

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { BiSolidMessageRoundedError } from "react-icons/bi";
 import { PrimaryButton } from "./Buttons/PrimaryButton";
 
 function ErrorPage() {
-  const router = useRouter();
   return (
     <div className="flex flex-col gap-6 w-full bg-gray-900 p-28 rounded-md text-gray-200 justify-center items-center">
       <p className="text-3xl font-bold text-green-500">Error encountered ...</p>
