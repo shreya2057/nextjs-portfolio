18a427a472625819b9eccd8922773ce2f36a1c85
shreya <shreyashrestha2k57@gmail.com>
2024-08-14 17:05:54 +0545

    feat: experience section

---
+++ src/app/(screens)/experience/page.tsx
@@ -0,0 +1,32 @@
+import { experience } from "@/nextjs-portfolio/data/resume";
+
+function Experience() {
+  return (
+    <div className="flex flex-col justify-center w-[90%] lg:w-[70%] bg-gray-700 p-10 rounded-md text-gray-200 gap-6">
+      <div className="flex flex-col gap-2">
+        <p className="text-2xl font-bold">Experience</p>
+        <hr className="flex w-48 border-gray-300 border-[1.5px]" />
+      </div>
+      <div className="flex flex-col gap-6">
+        {experience.map(({ id, position, companyName, timeline, details }) => (
+          <div className="flex flex-col gap-2" key={id}>
+            <p className="font-bold text-lg">{position}</p>
+            <div className="text-sm flex flex-col gap-2 sm:flex-row justify-between">
+              <p className="font-semibold">{companyName}</p>
+              <p>{timeline}</p>
+            </div>
+            <ul className="flex flex-col list-disc pl-6 gap-2">
+              {details.map((detail) => (
+                <li key={detail} className="text-justify text-sm">
+                  {detail}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import { IoLogoLinkedin, IoLogoGithub, IoLogoInstagram } from "react-icons/io5";
 
 export const Footer = () => {
   return (
-    <div className="flex flex-row py-6 px-16 justify-between items-center text-lime-500 ">
-      <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4 lg:flex-row py-6 px-10 justify-between items-center text-lime-500 ">
+      <div className="flex flex-col gap-2 self-start">
         <span className="font-semibold">Connect with me</span>
         <div className="grid grid-cols-3 gap-4">
           <Link href={"/"}>
@@ -25,7 +25,7 @@ export const Footer = () => {
         </div>
       </div>
       <div className="flex gap-6 font-semibold  items-center">
-        &#169; Shreya Shrestha, 2024
+        &#169; 2024 Shreya Shrestha. All rights reserved
       </div>
     </div>
   );
