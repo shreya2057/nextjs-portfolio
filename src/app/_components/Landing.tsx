f6d3ecaed540a1c5135655a3826eee1137f81183
shreya <shreyashrestha2k57@gmail.com>
2024-08-15 17:24:47 +0545

    fix: landing page issue fixes

--- src/app/_components/Landing.tsx
+++ src/app/_components/Landing.tsx
@@ -9,8 +9,8 @@ export const Landing = () => {
   const router = useRouter();
 
   return (
-    <div className="flex bg-gray-800 px-10 lg:px-40 py-10 relative overflow-hidden w-full h-[85dvh]">
-      <div className="flex flex-col md:flex-row lg:justify-between w-full gap-20 ">
+    <div className="flex bg-gray-800 px-10 lg:px-40 py-10 relative overflow-hidden w-full lg:h-[85dvh]">
+      <div className="flex flex-col md:flex-row lg:justify-between w-full gap-28 lg:gap-20 ">
         <div className="flex md:flex-[65%] gap-5 flex-col justify-center">
           <div className="flex items-center gap-3 text-amber-300 text-sm font-bold bg-amber-200 px-4 py-1 bg-opacity-25 rounded-md max-w-min text-nowrap flex-nowrap">
             <span>Hi! I am Shreya Shrestha</span>
@@ -29,7 +29,7 @@ export const Landing = () => {
           />
         </div>
         <div className="md:flex-[35%] flex md:justify-center items-center">
-          <div className="md:w-48 md:h-48 relative w-full flex justify-center">
+          <div className="md:w-48 md:h-48 relative w-full h-56 flex justify-center">
             <AiFillCode className="absolute text-[200px] text-lime-500 sel" />
             <TbMessageCode className="text-lime-500 text-[100px] md:text-4xl absolute -top-20 md:-top-4 right-3 md:-right-4 opacity-35" />
           </div>
